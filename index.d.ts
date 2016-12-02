075a3edca9b49519df615c7c3dafea9e0feb00e7
ghostnumber7 <marcogodoy@untitled.cl>
2016-12-02 15:28:44 -0300

    typings

---
+++ index.d.ts
@@ -0,0 +1,14 @@
+declare module "conduit" {
+  interface ConduitOptionsInterface {
+    steps?: [Conduit|Function]
+    setup?: any
+    reducer?: Function
+  }
+
+  class Conduit {
+    constructor(options: ConduitOptionsInterface)
+    run(input?: any): Promise<any>
+  }
+  namespace Conduit {}
+  export = Conduit
+}
